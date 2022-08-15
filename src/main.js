08f215740132baa5ba55f7f508a1c0aa45fc80d2
idellia <idellia83@gmail.com>
2022-08-15 23:15:35 +0900

    JS 수정

--- src/main.js
+++ src/main.js
@@ -11,6 +11,10 @@ import VModal from 'vue-js-modal';
 
 // 전역 Component
 import FullPageTitle from './components/Title/FullPageTitle';
+import Tooltip from '@/components/Tooltip/Tooltip';
+import Checkbox from '@/components/Checked/Checkbox';
+import Radio from '@/components/Checked/Radio';
+
 
 Vue.use(Toast);
 Vue.use(VModal, { dynamic: true });
@@ -24,6 +28,9 @@ Vue.config.errorHandler = (e) => {
 
 // 전역 Component
 Vue.component('full-page-title', FullPageTitle);
+Vue.component('tooltip', Tooltip);
+Vue.component('checkbox', Checkbox);
+Vue.component('radio', Radio);
 
 new Vue({
   vuetify,
