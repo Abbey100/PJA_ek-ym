b4fadba0c401bec2593fbdc687b4a667b46f4014
idellia <idellia83@gmail.com>
2022-08-15 21:03:08 +0900

    전역 컴포넌트 추가 - checkbox, radio : 로컬 코드를 전역 컴포넌트로 수정 - dialog : 전역 스타일로 변경 - select & dropdown : 전역 스타일로 변경 - tooltip : 전역으로 구성

---
+++ src/components/Checked/Checkbox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="checked-group d-flex justify-start">
+    <div class="checkbox-control">
+      <input type="checkbox" id="">
+      <label for=""></label>
+    </div>
+    <slot name="checkedContent"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Checkbox',
+}
+</script>
+
+<style src="./Checked.scss" lang="scss"/>
+
