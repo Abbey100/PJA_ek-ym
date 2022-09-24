a2f5c4adc7d7cdc6fef98b3a9d6a9e7ecb5ae93f
백은경 <ek100lovej@gmail.com>
2022-09-24 12:01:52 +0900

    오픈 프로젝트용 헤더 및 레이아웃 추가

---
+++ src/components/Layout/Layout2.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app class="pa-6">
+        <Header2 />
+        <GNB />
+        <v-main class="content">
+            <router-view />
+<!--            <Footer />-->
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+    import Header2 from '@/components/Header/Header2';
+    import GNB from '@/components/GNB/GNB';
+
+    // import Sidebar from '@/components/Sidebar/Sidebar';
+    // import Footer from "@/components/Footer/Footer";
+
+    export default {
+        name: 'Layout2',
+        // components: {Footer, Header, Sidebar },
+        components: {Header2, GNB },
+    };
+</script>
+
+<style src="./Layout.scss" lang="scss" />
